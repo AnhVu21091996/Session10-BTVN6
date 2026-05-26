a55b32b2c39b73900f5fba3a09f2164451643167
AnhVu21091996 <anhvuphan837@gmail.com>
2026-05-26 13:40:53 +0700

    BTVN6JS

---
+++ BTVN6JS.js
@@ -0,0 +1,5 @@
+let math = Number(prompt('Điểm Toán: '));
+let physics = Number(prompt('Điểm Lý: '));
+let chemistry = Number(prompt('Điểm Hóa: '));
+
+alert('Điểm trung bình: ' + (math + physics + chemistry)/3);
